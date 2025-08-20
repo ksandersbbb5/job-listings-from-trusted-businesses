6c531ad6a8d7326b4c23da3f5e28521f961e3705
ksandersbbb5 <ksanders@boston.bbb.org>
2025-08-20 08:09:43 -0400

    Delete pages/index.tsx

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-// pages/index.tsx
-export default function Index() {
-  return (
-    <main style={{ display: 'grid', placeItems: 'center', minHeight: '70vh' }}>
-      <h1>BBB Jobs — Pages Router Fallback</h1>
-    </main>
-  );
-}
