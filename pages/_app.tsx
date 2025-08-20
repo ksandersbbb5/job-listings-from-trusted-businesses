8266aa15f8a967b9dbcd6baaac72d4f45ad335f0
ksandersbbb5 <ksanders@boston.bbb.org>
2025-08-20 09:34:13 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '@/styles/globals.css';
+import '../styles/globals.css'; // relative path (works without TS path aliases)
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
