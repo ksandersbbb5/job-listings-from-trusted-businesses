3abb5724f81456a106a37d2bac94a8ac07e79603
ksandersbbb5 <ksanders@boston.bbb.org>
2025-08-20 08:04:23 -0400

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+// pages/index.tsx
+export default function Index() {
+  return (
+    <main style={{ display: 'grid', placeItems: 'center', minHeight: '70vh' }}>
+      <h1>BBB Jobs — Pages Router Fallback</h1>
+    </main>
+  );
+}
