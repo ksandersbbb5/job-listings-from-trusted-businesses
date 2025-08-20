8f883304bc792a1190f6089697f7ecedb0527966
ksandersbbb5 <ksanders@boston.bbb.org>
2025-08-20 10:37:07 -0400

    Delete pages/index.jsx

--- pages/index.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{display:'grid',placeItems:'center',minHeight:'60vh',fontFamily:'system-ui'}}>
-      <h1>Homepage is working.</h1>
-    </main>
-  );
-}
