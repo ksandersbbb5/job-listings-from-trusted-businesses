feff0fe7af57ae146a450b1a16ef182caa170b8e
ksandersbbb5 <ksanders@boston.bbb.org>
2025-08-20 10:11:41 -0400

    Delete pages/_app.tsx

--- pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.css'; // relative path (works without TS path aliases)
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
